13aeb9ab254ea8bb1e3e87c87e71d036b1b60a10
Suresh BOLLINA <suresh.bollina@amadeus.com>
2019-06-07 21:06:04 +0530

    gitdemo

---
+++ homepage.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="..\\jars\\chromedriver.exe")
+driver.get("http://www.liveupindia.com/")
+address_button = driver.find_element_by_xpath("//*[@class='btn btn-link btn-md dropdown-toggle search-location-btn']/span[2]")
+#print(address_button.is_enabled())
+#print(address_button.is_displayed())
+address_button.click()
+#print("====================================")
+city_list = driver.find_elements_by_xpath("//*[@class='dropdown-menu applyfilter project-type search-location']/li")
+#print(len(city_list))
+for city in city_list:
+    city_to_select = city.text.strip()
+    if city_to_select == "Pune":
+        print(city.text.strip(), " is selected")
+        city.click()
+        break
+    print("selected")
+driver.quit()
